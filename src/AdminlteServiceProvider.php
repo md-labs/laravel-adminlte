bcb92729c55ef91de3d1a24e90aae76aef3a566f
FlyingHail <flyinghail@msn.com>
2015-05-21 13:47:26 +0800

    Fix resource publish error

--- src/AdminlteServiceProvider.php
+++ src/AdminlteServiceProvider.php
@@ -31,7 +31,7 @@ class AdminlteServiceProvider extends ServiceProvider {
 
         $this->publishes([
             base_path().'/vendor/almasaeed2010/adminlte/' => public_path('packages/almasaeed2010/adminlte/'),
-            base_path().'/vendor/gionnivaleriana/laravel-adminlte/' => public_path('packages/gionnivaleriana/adminlte/'),
+            base_path().'/vendor/gionnivaleriana/laravel-adminlte/resources/' => public_path('packages/gionnivaleriana/adminlte/'),
             base_path().'/vendor/sleeping-owl/admin/resources/css/admin.css' => public_path('packages/gionnivaleriana/adminlte/css/admin.css'),
             base_path().'/vendor/sleeping-owl/admin/resources/js/admin.js' => public_path('packages/gionnivaleriana/adminlte/js/admin.js'),
         ], 'assets');
